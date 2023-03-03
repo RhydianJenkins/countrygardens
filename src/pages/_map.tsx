b6bcc3a9103ca9119a8faa70da8c8b8899ee87d6
Rhydian Jenkins <rhydz@msn.com>
2023-03-03 01:07:47 +0000

    Add google maps to home page

--- src/pages/_map.tsx
+++ src/pages/_map.tsx
@@ -1,17 +1,80 @@
-import { Box } from '@mui/material';
+import React, { useEffect, useRef, ReactElement, ReactNode, useState } from "react";
+import { Loader, LoaderOptions } from '@googlemaps/js-api-loader';
 
-export default function Map() {
+enum Status {
+  LOADING = 'LOADING',
+  FAILURE = 'FAILURE',
+  SUCCESS = 'SUCCESS',
+}
+
+export interface WrapperProps extends LoaderOptions {
+  children?: ReactNode;
+  render?: (status: Status) => ReactElement;
+  callback?: (status: Status, loader: Loader) => void;
+}
+
+const render = (status: Status) => {
+  if (status === Status.LOADING) return <h3>{status} ..</h3>;
+  if (status === Status.FAILURE) return <h3>{status} ...</h3>;
+};
+
+function MyMapComponent({
+  center,
+  zoom,
+}: {
+  center: google.maps.LatLngLiteral;
+  zoom: number;
+}) {
+  const ref = useRef<HTMLElement>();
+
+  useEffect(() => {
+    new window.google.maps.Map(ref.current!, {
+      center,
+      zoom,
+    });
+  });
+
+  return <div ref={ref} id="map" />;
+}
+
+const Wrapper = ({
+  children,
+  render,
+  callback,
+  ...options
+}: WrapperProps): ReactElement => {
+  const [status, setStatus] = useState(Status.LOADING);
+
+  useEffect(() => {
+    const loader = new Loader(options);
+
+    const setStatusAndExecuteCallback = (status: Status) => {
+      if (callback) callback(status, loader);
+      setStatus(status);
+    };
+
+    setStatusAndExecuteCallback(Status.LOADING);
+
+    loader.load().then(
+      () => setStatusAndExecuteCallback(Status.SUCCESS),
+      () => setStatusAndExecuteCallback(Status.FAILURE)
+    );
+  }, [callback, options]);
+
+  if (status === Status.SUCCESS && children) return <>{children}</>;
+
+  if (render) return render(status);
+
+  return <></>;
+};
+
+export default function SimpleMap(){
+  const center = { lat: -34.397, lng: 150.644 };
+  const zoom = 4;
+
   return (
-    <Box sx={{
-      width: '100vw',
-      backgroundColor: 'primary.main',
-      margin: 'auto',
-      padding: '16px',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}>
-      Google Map...
-    </Box>
+    <Wrapper apiKey={process.env.GOOGLE_MAPS_API_KEY || ''} render={render}>
+      <MyMapComponent center={center} zoom={zoom} />
+    </Wrapper>
   );
 }
