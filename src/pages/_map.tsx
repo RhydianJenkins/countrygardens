e27e5b3f98c0215da218c1a0bd46faa870a8494a
Rhydian Jenkins <RhydianJenkins@users.noreply.github.com>
2023-03-03 17:11:41 +0000

    Add google maps to home page (#1)

--- src/pages/_map.tsx
+++ src/pages/_map.tsx
@@ -1,17 +1,51 @@
-import { Box } from '@mui/material';
+import React from 'react'
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import { Box, CircularProgress } from '@mui/material';
 
-export default function Map() {
-  return (
-    <Box sx={{
-      width: '100vw',
-      backgroundColor: 'primary.main',
-      margin: 'auto',
-      padding: '16px',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}>
-      Google Map...
-    </Box>
-  );
+function Map() {
+    const { isLoaded, loadError } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || '',
+    })
+
+    if (loadError) {
+        console.error('Error loading Google Maps API');
+        return <></>;
+    }
+
+    const center = {
+        lat: 50.73096646682943,
+        lng: -0.7930198428999052,
+    };
+
+    if (isLoaded) {
+        return (
+            <GoogleMap
+                mapContainerStyle={{
+                    width: '100%',
+                    height: '1024px',
+                }}
+                center={center}
+                zoom={17}
+            >
+                <Marker
+                    position={center}
+                    title="Country Gardens"
+                />
+            </GoogleMap>
+        );
+    }
+
+    return (
+        <Box sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '1024px'
+        }}>
+            <CircularProgress />
+        </Box>
+    )
 }
+
+export default React.memo(Map)
