6cdecdba6d882f3e7f035270298cd89485cb83fd
Rhydian Jenkins <rhydz@msn.com>
2023-03-03 23:50:54 +0000

    Use products instead of orders

--- src/pages/_products.tsx
+++ src/pages/_products.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Box } from '@mui/material';
+import { Product as ProductEntity } from '@/database';
 import Product from './_product';
-import { Order } from '@/database';
 
 function Products() {
-    const [orders, setOrders] = React.useState<Order[]>([]);
+    const [products, setProducts] = React.useState<ProductEntity[]>([]);
     const [loading, setLoading] = React.useState(false);
     const [error, setError] = React.useState(false);
 
     React.useMemo(() => {
         setLoading(true);
 
-        fetch('/api/orders')
+        fetch('/api/products')
             .then((res) => res.json())
-            .then((orders) => setOrders(orders))
+            .then((products) => setProducts(products))
             .catch(() => setError(true))
             .finally(() => setLoading(false));
     }, []);
@@ -26,7 +26,7 @@ function Products() {
         return <h1>Something went wrong...</h1>;
     }
 
-    console.log(orders);
+    console.log(products);
 
     return (
         <Box sx={{
@@ -37,11 +37,11 @@ function Products() {
             display: 'flex',
             gap: '32px',
         }}>
-            {orders.map((order, index) =>
+            {products.map((product, index) =>
                 <Product
                     key={index}
-                    name={order.name}
-                    value={order.value}
+                    name={product.name}
+                    value={product.value}
                 />
             )}
         </Box>
