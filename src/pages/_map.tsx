3b4b96e40b6328cce4b79ca0ee8b084d5fdab204
Rhydian Jenkins <rhydz@msn.com>
2023-03-03 16:57:58 +0000

    Working

--- src/pages/_map.tsx
+++ src/pages/_map.tsx
@@ -1,80 +1,42 @@
-import React, { useEffect, useRef, ReactElement, ReactNode, useState } from "react";
-import { Loader, LoaderOptions } from '@googlemaps/js-api-loader';
+import React from 'react'
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 
-enum Status {
-  LOADING = 'LOADING',
-  FAILURE = 'FAILURE',
-  SUCCESS = 'SUCCESS',
-}
 
-export interface WrapperProps extends LoaderOptions {
-  children?: ReactNode;
-  render?: (status: Status) => ReactElement;
-  callback?: (status: Status, loader: Loader) => void;
-}
+function MyComponent() {
+    const { isLoaded, loadError } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || '',
+    })
 
-const render = (status: Status) => {
-  if (status === Status.LOADING) return <h3>{status} ..</h3>;
-  if (status === Status.FAILURE) return <h3>{status} ...</h3>;
-};
+    if (loadError) {
+        console.error(loadError)
+        return <div>Map cannot be loaded right now, sorry.</div>
+    }
 
-function MyMapComponent({
-  center,
-  zoom,
-}: {
-  center: google.maps.LatLngLiteral;
-  zoom: number;
-}) {
-  const ref = useRef<HTMLElement>();
-
-  useEffect(() => {
-    new window.google.maps.Map(ref.current!, {
-      center,
-      zoom,
-    });
-  });
-
-  return <div ref={ref} id="map" />;
-}
-
-const Wrapper = ({
-  children,
-  render,
-  callback,
-  ...options
-}: WrapperProps): ReactElement => {
-  const [status, setStatus] = useState(Status.LOADING);
-
-  useEffect(() => {
-    const loader = new Loader(options);
-
-    const setStatusAndExecuteCallback = (status: Status) => {
-      if (callback) callback(status, loader);
-      setStatus(status);
+    const center = {
+        lat: 50.73096646682943,
+        lng: -0.7930198428999052,
     };
 
-    setStatusAndExecuteCallback(Status.LOADING);
+    if (isLoaded) {
+        return (
+            <GoogleMap
+                mapContainerStyle={{
+                    width: '100%',
+                    height: '1024px',
+                }}
+                center={center}
+                zoom={17}
+            >
+                <Marker
+                    position={center}
+                    title="Country Gardens"
+                />
+            </GoogleMap>
+        );
+    }
 
-    loader.load().then(
-      () => setStatusAndExecuteCallback(Status.SUCCESS),
-      () => setStatusAndExecuteCallback(Status.FAILURE)
-    );
-  }, [callback, options]);
-
-  if (status === Status.SUCCESS && children) return <>{children}</>;
-
-  if (render) return render(status);
-
-  return <></>;
-};
-
-export default function SimpleMap(){
-  const center = { lat: -34.397, lng: 150.644 };
-  const zoom = 4;
-
-  return (
-    <Wrapper apiKey={process.env.GOOGLE_MAPS_API_KEY || ''} render={render}>
-      <MyMapComponent center={center} zoom={zoom} />
-    </Wrapper>
-  );
+    return <></>;
 }
+
+export default React.memo(MyComponent)
