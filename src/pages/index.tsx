ad23bda4e13bdf0727a3bd5ca46b1de641fdb2c9
Rhydian Jenkins <rhydz@msn.com>
2023-03-06 19:07:05 +0000

    Use getStaticProps to 'fetch' products (fake data atm)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,27 @@
 import Cover from "@/components/cover";
 import Products from "@/components/products";
 import ContactUs from "@/components/contactUs";
+import { ProductEntity } from "./api/products";
 
-function Home() {
+export const getStaticProps = async () => {
+    // const res = await fetch("http://localhost:3000/api/data");
+    // const data = await res.json();
+    const products = [
+        { name: "Product 1", value: 69 },
+        { name: "Product 2", value: 420 },
+    ] as ProductEntity[];
+
+    return {
+        props: { products },
+        revalidate: 20,
+    };
+};
+
+function Home({ products }: { products: ProductEntity[] }) {
     return (
         <>
             <Cover />
-            <Products products={[]}/>
+            <Products products={products}/>
             <ContactUs />
         </>
     );
