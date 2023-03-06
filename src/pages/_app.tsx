dbba168513f636b23f950bdbb5f54417c5436436
Rhydian Jenkins <rhydz@msn.com>
2023-03-06 21:39:56 +0000

    Fix basket shift css issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,16 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from '@mui/material/styles';
 import Footer from '@/components/footer';
 import Header from '@/components/header';
+import { Box } from '@mui/material';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <ThemeProvider theme={theme}>
-            <Header />
-            <Component {...pageProps} />
-            <Footer />
+            <Box sx={{ width: '100vw' }}>
+                <Header />
+                <Component {...pageProps} />
+                <Footer />
+            </Box>
         </ThemeProvider>
     );
 }
