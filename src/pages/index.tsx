506e3c63b335600d31e428dda666e413fccbbb9e
Rhydian Jenkins <rhydz@msn.com>
2023-03-06 17:53:32 +0000

    Removed server side props. Fuck it.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,8 @@
 import Cover from "@/components/cover";
-import Products, { ProductsProps } from "@/components/products";
+import Products from "@/components/products";
 import ContactUs from "@/components/contactUs";
 
-export async function getServerSideProps() {
-    const products = [
-        { name: "Product 1", value: 69 },
-        { name: "Product 2", value: 420 },
-    ];
-
-    return {
-        props: { products },
-    };
-}
-
-function Home({ products }: ProductsProps) {
+function Home() {
     return (
         <>
             <Cover />
