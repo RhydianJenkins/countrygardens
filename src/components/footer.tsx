1b4c7a4cbc96734ed3d049ff96b5fde6c861e3c7
Rhydian Jenkins <rhydz@msn.com>
2023-03-07 23:15:35 +0000

    Add basket page

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,13 +4,14 @@ import { Facebook } from '@mui/icons-material';
 export default function Footer() {
     return (
         <Box sx={{
-            width: '80vw',
+            width: '100vw',
             backgroundColor: 'secondary.main',
             margin: 'auto',
-            padding: '16px',
+            padding: '1em',
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
+            height: '10em',
         }}>
             <a href="https://www.facebook.com/CountryGardensSelsey" target="_blank">
                 <Facebook />
--- src/pages/basket/index.tsx
+++ src/pages/basket/index.tsx
@@ -1,23 +1,38 @@
+'use client';
+
+import { Box } from "@mui/material";
+
 function BasketPage() {
     const basket = [
-        {
-            id: 1,
-            name: 'Product 1',
-            price: 'Lots!',
-        },
+        { id: 1, name: 'Product 1', price: 'Lots!' },
+        { id: 2, name: 'Product 2', price: 'Lots!' },
+        { id: 3, name: 'Product 3', price: 'Lots!' },
+        { id: 4, name: 'Product 4', price: 'Lots!' },
+        { id: 5, name: 'Product 5', price: 'Lots!' },
     ];
 
     return (
-        <div>
+        <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            marginTop: '10em',
+            alignItems: 'center',
+        }}>
             <h1>Basket</h1>
-            <ul>
-                {basket.map((item) => (
-                    <li key={item.id}>
-                        {item.name} - {item.price}
-                    </li>
-                ))}
-            </ul>
-        </div>
+            <Box sx={{
+                minHeight: 'calc(100vh - 22em)',
+                padding: '1em',
+                flexGrow: 1,
+            }}>
+                <ul>
+                    {basket.map((item) => (
+                        <li key={item.id}>
+                            {item.name} - {item.price}
+                        </li>
+                    ))}
+                </ul>
+            </Box>
+        </Box>
     );
 }
 
