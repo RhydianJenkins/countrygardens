645ab2be5c1b74fc60dfc3f0d778f26b43c3dd00
Rhydian Jenkins <rhydz@msn.com>
2023-03-08 17:05:07 +0000

    Cleanup code - still borked

--- src/components/basketIcon.tsx
+++ src/components/basketIcon.tsx
@@ -1,37 +1,28 @@
-import { Badge, IconButton } from '@mui/material';
+import { Badge } from '@mui/material';
 import { ShoppingBasket } from '@mui/icons-material';
 import React from 'react';
 import Tooltip from '@mui/material/Tooltip';
 import useBasket from '@/hooks/useBasket';
 import NextLink from 'next/link';
 
-function BasketIcon() {
+const BasketIcon = () => {
     const [basket] = useBasket();
     const [basketCount, setBasketCount] = React.useState(0);
 
-    const handleOpenBasket = () => {
-        // eslint-disable-next-line no-console
-        console.log('TODO navigate to basket');
-    };
-
     React.useEffect(() => {
-        // TODO this isn't updating after initial page load
-        // newCount is correct, but doesn't update
-        const newCount = Object.keys({ ...basket }).reduce((acc, key) => acc + basket[key], 0);
-        setBasketCount(newCount);
+        const basketCount = Object.keys(basket).reduce((acc, key) => acc + basket[key], 0);
+        setBasketCount(basketCount);
     }, [basket]);
 
     return (
         <NextLink href='/basket'>
             <Tooltip title="Go to Basket">
-                <IconButton onClick={handleOpenBasket} sx={{ p: 0 }}>
-                    <Badge color="secondary" badgeContent={basketCount}>
-                        <ShoppingBasket />
-                    </Badge>
-                </IconButton>
+                <Badge color="secondary" badgeContent={basketCount}>
+                    <ShoppingBasket />
+                </Badge>
             </Tooltip>
         </NextLink>
     );
-}
+};
 
 export default BasketIcon;
--- src/hooks/useBasket.ts
+++ src/hooks/useBasket.ts
@@ -8,30 +8,32 @@ type BasketReturnType = [
 ];
 
 function useBasket(): BasketReturnType {
-    const [basket, setBasket] = React.useState(() => {
-        if (typeof window === "undefined") {
-            return {};
+    const [basket, setBasket] = React.useState<Basket>(() => {
+        if (typeof window !== 'undefined') {
+            try {
+                const basketString = window.localStorage.getItem('basket') || '{}';
+                const basket = JSON.parse(basketString);
+
+                return basket;
+            } catch (error) {
+                // eslint-disable-next-line no-console
+                console.error(error);
+            }
         }
 
-        try {
-            const basketString = window.localStorage.getItem('basket') || '{}';
-            const basket = JSON.parse(basketString);
-            return basket;
-        } catch (error) {
-            // eslint-disable-next-line no-console
-            console.error(error);
-            return {};
-        }
+        return {};
     });
 
     const addItem = (id: string) => {
         try {
-            basket[id] = basket[id] ? basket[id] + 1 : 1;
+            const basketClone = { ...basket };
 
-            setBasket(basket);
+            basketClone[id] = (basketClone[id] || 0) + 1;
+
+            setBasket(basketClone);
 
             if (typeof window !== "undefined") {
-                window.localStorage.setItem('basket', JSON.stringify(basket));
+                window.localStorage.setItem('basket', JSON.stringify(basketClone));
             }
         } catch (error) {
             // eslint-disable-next-line no-console
@@ -43,26 +45,27 @@ function useBasket(): BasketReturnType {
         try {
             if (!(id in basket)) {
                 // eslint-disable-next-line no-console
-                console.warn(`${id} not in basket`);
+                console.warn(`Unable to remove ${id} from basket (not in basket)`);
                 return;
             }
 
-            basket[id] = basket[id] - number;
+            const basketClone = { ...basket };
 
-            if (basket[id] <= 0) {
-                delete basket[id];
+            basketClone[id] = basketClone[id] - number;
+
+            if (basketClone[id] <= 0) {
+                delete basketClone[id];
             }
 
-            setBasket(basket);
+            setBasket(basketClone);
 
             if (typeof window !== "undefined") {
-                window.localStorage.setItem('basket', JSON.stringify(basket));
+                window.localStorage.setItem('basket', JSON.stringify(basketClone));
             }
         } catch (error) {
             // eslint-disable-next-line no-console
             console.error(error);
         }
-
     };
 
     return [basket, addItem, removeItem];
