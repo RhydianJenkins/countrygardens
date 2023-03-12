38810eaac1807151a0faae098ba7bb0144997a9d
Rhydian Jenkins <rhydz@msn.com>
2023-03-12 23:23:43 +0000

    Tried using function syntax

--- src/pages/api/test/index.ts
+++ src/pages/api/test/index.ts
@@ -1,7 +1,7 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
 
-const handler = async (_req: VercelRequest, res: VercelResponse) => {
+async function handler(_req: VercelRequest, res: VercelResponse) {
     return res.status(200).json({ message: `Hello world!` });
-};
+}
 
 export default handler;
