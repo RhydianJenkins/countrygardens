0a8dd0aa5a8e74597fa71780b29fa83ef9d04929
Rhydian Jenkins <rhydz@msn.com>
2023-03-12 23:11:38 +0000

    Try vercel/node api test

--- src/pages/api/test/index.ts
+++ src/pages/api/test/index.ts
@@ -1,8 +1,7 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { VercelRequest, VercelResponse } from "@vercel/node";
 
-const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
-    res.status(200).json({ message: 'Hello, world' });
-    res.end();
+const handler = async (_req: VercelRequest, res: VercelResponse) => {
+    return res.status(200).json({ message: `Hello world!` });
 };
 
 export default handler;
