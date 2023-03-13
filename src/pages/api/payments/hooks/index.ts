6706ea7beb495a2fa69c03e5ceba0fba1fb05cc9
Rhydian Jenkins <rhydz@msn.com>
2023-03-13 01:00:21 +0000

    Initial webhooks work

--- src/pages/api/payments/hooks/index.ts
+++ src/pages/api/payments/hooks/index.ts
@@ -1,6 +1,10 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import Stripe from 'stripe';
 
+if (!process.env.STRIPE_WEBHOOK_SECRET) {
+    throw new Error('Missing STRIPE_WEBHOOK_SECRET environment variable.');
+}
+
 const webhookHandlers = {
     'payment_intent.succeeded': async (paymentIntent: Stripe.PaymentIntent) => {
         // Add your business logic here
@@ -14,5 +18,20 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    res.status(400).send('Webhook not yet implemented');
+    const stripeSignature = req.headers['stripe-signature'];
+    const event = Stripe.webhooks.constructEvent(
+        req.body,
+        stripeSignature,
+        process.env.STRIPE_WEBHOOK_SECRET
+    );
+
+    try {
+        await webhookHandlers[event.type](event.data.object);
+        res.status(200).json({ received: true });
+    } catch (err: any) {
+        // eslint-disable-next-line no-console
+        console.error(err);
+
+        res.status(400).send(`Webhook Error: ${err?.message || 'unknown'}`);
+    }
 }
