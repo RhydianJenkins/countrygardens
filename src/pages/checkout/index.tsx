ad252940b8b56ed99e45483ca2d75adf2d799d3b
Rhydian Jenkins <RhydianJenkins@users.noreply.github.com>
2023-03-14 22:33:01 +0000

    Stripe sessions (#13)

--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -1,24 +1,20 @@
 import Basket from "@/components/basket";
 import { formatUnitAmount } from "@/components/product";
+import StripeConfirmation from "@/components/stripeConfirmation";
 import { BasketContext, BasketType } from "@/hooks/useBasket";
 import { getProducts, ProductEntity } from "@/pages/api/products";
-import { Button as MuiButton, Box, Step, StepLabel, Stepper, Typography, CircularProgress, Snackbar, Alert } from "@mui/material";
+import { Button as MuiButton, Box, Step, StepLabel, Stepper, Typography, CircularProgress, Snackbar, Alert, AlertColor } from "@mui/material";
 import React from "react";
-import StripePaymentFields, { createPaymentIntent, handlePayment } from "@/components/stripe";
-import { PaymentIntent, Stripe, StripeElements, StripeError } from "@stripe/stripe-js";
 import { useRouter } from 'next/router';
-import StripeConfirmation from "@/components/stripeConfirmation";
+
+type StepControlsProps = {
+    actionButtonText?: string,
+    submitButtonDisabled?: boolean,
+    submitButtonLoading?: boolean,
+}
 
 type CheckoutPageProps = {
     allProducts: ProductEntity[];
-}
-
-type StepControlsProps = {
-    basket: BasketType;
-    handleBack: () => void;
-    activeStep: number;
-    priceString: string;
-    nextButtonLoading?: boolean;
 }
 
 export const getStaticProps = async () => {
@@ -32,31 +28,36 @@ export const getStaticProps = async () => {
 
 const steps = ['Basket', 'Payment', 'Confirmation'];
 
-function getProgressButtonText({ activeStep, priceString }: {
-    activeStep: number,
-    priceString: string
-}) {
-    if (activeStep === 0) {
-        return 'Go to payment';
+const createCheckoutSession = async (basket: BasketType) => {
+    try {
+        const response = await fetch(
+            'api/payments/checkout',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': "application/json",
+                },
+                body: JSON.stringify({
+                    basket,
+                }),
+            }
+        );
+
+        if (response.status !== 201) {
+            throw new Error('Failed to create checkout session');
+        }
+
+        const { url } = await response.json();
+        window.location.href = url;
+    } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error('Rejecting promise with error', error);
+
+        return Promise.reject(error);
     }
+};
 
-    if (activeStep === 1) {
-        return `Pay ${priceString}`;
-    }
-
-    return 'Got it';
-}
-
-function StepControls({ basket, handleBack, activeStep, priceString, nextButtonLoading }: StepControlsProps) {
-    const [nextDisabled, setNextDisabled] = React.useState(false);
-
-    React.useEffect(() => {
-        const basketIsEmpty = Object.keys(basket).length === 0;
-        const isOnPaymentStep = activeStep === 2;
-
-        setNextDisabled(basketIsEmpty && !isOnPaymentStep);
-    }, [basket]);
-
+function StepControls({ submitButtonDisabled, submitButtonLoading, actionButtonText = 'Next' }: StepControlsProps) {
     return (
         <Box sx={{
             display: 'flex',
@@ -64,25 +65,18 @@ function StepControls({ basket, handleBack, activeStep, priceString, nextButtonL
             padding: '1em',
             margin: '1em',
         }}>
-            {activeStep === 1 && <MuiButton
-                onClick={handleBack}
-                variant='contained'
-            >
-                Back
-            </MuiButton>}
-
             <Box sx={{ flex: '1 1 auto' }} />
 
             <MuiButton
                 type='submit'
                 variant="contained"
-                disabled={nextDisabled || nextButtonLoading}
+                disabled={submitButtonDisabled || submitButtonLoading}
                 sx={{
                     backgroundColor: 'secondary.main',
                 }}
             >
-                {nextButtonLoading && <CircularProgress size={'2em'} sx={{ marginRight: '1em' }}/>}
-                <Typography>{getProgressButtonText({ activeStep, priceString })}</Typography>
+                {submitButtonLoading && <CircularProgress size={'2em'} sx={{ marginRight: '1em' }}/>}
+                <Typography>{actionButtonText}</Typography>
             </MuiButton>
         </Box>
     );
@@ -90,18 +84,31 @@ function StepControls({ basket, handleBack, activeStep, priceString, nextButtonL
 
 function CheckoutPage({ allProducts }: CheckoutPageProps) {
     const { basket, clearBasket } = React.useContext(BasketContext);
-    const [activeStep, setActiveStep] = React.useState(0);
     const [totalBasketCost, setTotalBasketCost] = React.useState(0);
-    const [paymentIntent, setPaymentIntent] = React.useState<PaymentIntent|null>(null);
-    const [stripe, setStripe] = React.useState<Stripe|null>(null);
-    const [elements, setElements] = React.useState<StripeElements|null>(null);
     const [loading, setLoading] = React.useState(false);
     const [snackbarOpen, setSnackbarOpen] = React.useState(false);
     const [snackbarMessage, setSnackbarMessage] = React.useState('');
-    const [customerEmail, setCustomerEmail] = React.useState('');
+    const [success, setSuccess] = React.useState(false);
+    const [snackbarAlertColor, setSnackbarAlertColor] = React.useState<AlertColor>('success');
     const router = useRouter();
-
     const priceString = formatUnitAmount(totalBasketCost);
+    const [actionButtonText, setActionButtonText] = React.useState('Next');
+
+    const showSnackbar = (message: string, alertColor: AlertColor = 'info') => {
+        setSnackbarMessage(message);
+        setSnackbarAlertColor(alertColor);
+        setSnackbarOpen(true);
+    };
+
+    const getActionButtonText = (): string => {
+        const query = new URLSearchParams(window.location.search);
+
+        if (query.get('success') || Object.keys(basket).length === 0) {
+            return 'Back to home';
+        }
+
+        return 'Checkout';
+    };
 
     React.useEffect(() => {
         const basketPrices = Object.entries(basket).map(([id, number]) => {
@@ -118,68 +125,32 @@ function CheckoutPage({ allProducts }: CheckoutPageProps) {
         setTotalBasketCost(newTotalBasketCost);
     }, [basket]);
 
-    const handleBack = () => {
-        if (activeStep === 0) {
-            // eslint-disable-next-line no-console
-            console.warn('Attempted to go back from first step');
-            return;
+    React.useEffect(() => {
+        const query = new URLSearchParams(window.location.search);
+        if (query.get('success')) {
+            showSnackbar('Order placed! You will receive an email confirmation.', 'success');
+            setSuccess(true);
+            clearBasket();
         }
 
-        setActiveStep((prevActiveStep) => prevActiveStep - 1);
-    };
-
-    const fetchNewPaymentIntent = async () => {
-        try {
-            const paymentIntent = await createPaymentIntent({ basket });
-            setPaymentIntent(paymentIntent);
-        } catch (error) {
-            setSnackbarMessage('Sorry, something went wrong');
-            setSnackbarOpen(true);
-
-            // eslint-disable-next-line no-console
-            console.error(error);
-            throw new Error('Failed to create stripe payment intent');
+        if (query.get('canceled')) {
+            showSnackbar('Order cancelled. You will receive an email confirmation.', 'info');
         }
-    };
 
-    const onPaymentComplete = (updatedPaymentIntent: PaymentIntent) => {
-        setPaymentIntent(updatedPaymentIntent);
-        setActiveStep((prevActiveStep) => prevActiveStep + 1);
-        clearBasket();
-    };
-
-    const onPaymentError = ({ message }: StripeError) => {
-        setSnackbarMessage(message || 'Sorry, something went wrong');
-        setSnackbarOpen(true);
-    };
+        setActionButtonText(getActionButtonText());
+    }, []);
 
     const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
 
-        switch (activeStep) {
-        case 0:
-            setLoading(true);
-            fetchNewPaymentIntent();
-            setActiveStep((prevActiveStep) => prevActiveStep + 1);
-            setLoading(false);
-            break;
-        case 1:
-            setLoading(true);
-            await handlePayment({
-                stripe,
-                elements,
-                onPaymentComplete,
-                onPaymentError,
-                customerEmail,
-            });
-            setLoading(false);
-            break;
-        case 2:
+        if (success || Object.keys(basket).length === 0) {
             router.push('/');
             return;
-
-        default: throw new Error('Unknown checkout step');
         }
+
+        setLoading(true);
+        await createCheckoutSession(basket);
+        setLoading(false);
     };
 
     return (
@@ -191,62 +162,40 @@ function CheckoutPage({ allProducts }: CheckoutPageProps) {
             >
                 <Alert
                     onClose={() => setSnackbarOpen(false)}
-                    severity="error"
+                    severity={snackbarAlertColor}
                 >
                     {snackbarMessage}
                 </Alert>
             </Snackbar>
 
-            <Box
-                paddingLeft={{ xs: '0', md: '10em' }}
-                paddingRight={{ xs: '0', md: '10em' }}
-                maxWidth={{ xs: '100%', md: '80em' }}
-                paddingTop='10em'
-                margin='auto'
-            >
-                <Stepper
-                    sx={{
-                        marginBottom: '5em',
-                    }}
-                    activeStep={activeStep}
-                    alternativeLabel
+            <form onSubmit={onSubmit}>
+                <Box
+                    paddingLeft={{ xs: '0', md: '10em' }}
+                    paddingRight={{ xs: '0', md: '10em' }}
+                    maxWidth={{ xs: '100%', md: '80em' }}
+                    paddingTop='10em'
+                    margin='auto'
                 >
-                    {steps.map((label) => {
-                        const stepProps: { completed?: boolean } = {};
-                        const labelProps: { optional?: React.ReactNode; } = {};
+                    <Stepper sx={{ marginBottom: '5em' }} alternativeLabel >
+                        {steps.map((label) => {
+                            const stepProps: { completed?: boolean } = {};
+                            const labelProps: { optional?: React.ReactNode; } = {};
 
-                        return (
-                            <Step key={label} {...stepProps}>
-                                <StepLabel {...labelProps}>{label}</StepLabel>
-                            </Step>
-                        );
-                    })}
-                </Stepper>
+                            return (
+                                <Step key={label} {...stepProps}>
+                                    <StepLabel {...labelProps}>{label}</StepLabel>
+                                </Step>
+                            );
+                        })}
+                    </Stepper>
 
-                <form noValidate onSubmit={(test) => onSubmit(test)}>
-                    {activeStep === 0 && <Basket
-                        allProducts={allProducts}
-                        totalPrice={priceString}
-                    />}
+                    {success && <StripeConfirmation />}
 
-                    {activeStep === 1 && <StripePaymentFields
-                        clientSecret={paymentIntent?.client_secret || null}
-                        setStripe={setStripe}
-                        setElements={setElements}
-                        setCustomerEmail={setCustomerEmail}
-                    />}
+                    {!success && <Basket allProducts={allProducts} totalPrice={priceString} />}
 
-                    {activeStep === 2 && <StripeConfirmation paymentIntent={paymentIntent} />}
-
-                    <StepControls
-                        basket={basket}
-                        handleBack={handleBack}
-                        activeStep={activeStep}
-                        priceString={priceString}
-                        nextButtonLoading={loading}
-                    />
-                </form>
-            </Box>
+                    <StepControls actionButtonText={actionButtonText} submitButtonLoading={loading} />
+                </Box>
+            </form>
         </>
     );
 
